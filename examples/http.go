adabceae13bcb2076fbfcc8ddbb37a450c1903f9
Hoanh An <hoanhan@bennington.edu>
2019-01-24 13:13:26 +0700

    Add in some examples

---
+++ examples/http.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/vapor-ware/synse-client-go/synse"
+)
+
+func main() {
+	config := &synse.Options{
+		Server: synse.ServerOptions{
+			Address: "localhost:5000",
+		},
+	}
+
+	client, err := synse.NewHTTPClient(config)
+	fmt.Printf("%+v, %+v\n", client, err)
+
+	r1, err := client.Status()
+	fmt.Printf("%+v, %+v\n", r1, err)
+
+	r2, err := client.Version()
+	fmt.Printf("%+v, %+v\n", r2, err)
+
+	r3, err := client.Config()
+	fmt.Printf("%+v, %+v\n", r3, err)
+}
