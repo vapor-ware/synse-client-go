4161f61f597e89b59a666a91445844caec39ed8e
Hoanh An <hoanhan@bennington.edu>
2019-01-25 18:07:57 +0700

    Add docstring for retry backoff config

--- synse/config.go
+++ synse/config.go
@@ -8,7 +8,7 @@ import (
 
 // Options is the root config options.
 type Options struct {
-	// Address specifies the URL of Synse Server in format host[:port]
+	// Address specifies the URL of Synse Server in the format `host[:port]`.
 	Address string
 
 	// Timeout specifies a time limit for a request.
@@ -18,10 +18,20 @@ type Options struct {
 	Retry RetryOptions
 }
 
-// RetryOptions is the config options for backoff retry mechanism.
-// TODO - add docs for all the fields.
+// RetryOptions is the config options for backoff retry mechanism. Its strategy
+// is to increase retry intervals after each failed attempt, until some maximum
+// value.
 type RetryOptions struct {
-	Count       int
-	WaitTime    time.Duration
+	// Count specifies the number of retry attempts. Zero value means no retry.
+	Count int
+
+	// WaitTime specifies the wait time before retrying request. It is
+	// increased after each attempt.
+	// Default is 100 milliseconds.
+	WaitTime time.Duration
+
+	// MaxWaitTime specifies the maximum wait time, the cap, of all retry
+	// requests that are made.
+	// Default is 2 seconds
 	MaxWaitTime time.Duration
 }
