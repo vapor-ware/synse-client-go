09b12d43d9fa82e5604efa80643a5cef18cd3e54
Hoanh An <hoanhan@bennington.edu>
2019-02-27 08:47:49 -0500

    Set up tls for websocket

--- synse/websocket.go
+++ synse/websocket.go
@@ -3,19 +3,64 @@ package synse
 // websocket.go implements a websocket client.
 
 import (
+	"crypto/tls"
+
+	"github.com/gorilla/websocket"
+	"github.com/pkg/errors"
 	"github.com/vapor-ware/synse-client-go/synse/scheme"
 )
 
 type websocketClient struct {
-	// TODO
+	// options is the global config options of the client.
+	options *Options
+
+	// client holds the websocket.Dialer.
+	client *websocket.Dialer
+
+	// apiVersion is the current api version of Synse Server that we are
+	// communicating with.
+	apiVersion string
 }
 
-func NewWebSocketClientV3(options *Options) (Client, error) {
-	return nil, nil
+// NewWebSocketClientV3 returns a new instance of a websocket client for v3.
+func NewWebSocketClientV3(opts *Options) (Client, error) {
+	c, err := createWebSocketClient(opts)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create a websocket client")
+	}
+
+	return &websocketClient{
+		options:    opts,
+		client:     c,
+		apiVersion: "v3",
+	}, nil
 }
 
-func createWebSocketClient(opt *Options) error {
-	return nil
+// createWebSocketClient setups a websocket dialer with configured options.
+func createWebSocketClient(opts *Options) (*websocket.Dialer, error) {
+	err := setDefaults(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if opts.TLS.Enabled == false {
+		return &websocket.Dialer{
+			HandshakeTimeout: opts.WebSocket.HandshakeTimeout,
+		}, nil
+	}
+
+	// Only return a TLS client if its config option is enable.
+	cert, err := setTLS(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return &websocket.Dialer{
+		HandshakeTimeout: opts.WebSocket.HandshakeTimeout,
+		TLSClientConfig: &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		},
+	}, nil
 }
 
 // Status returns the status info. This is used to check if the server
@@ -54,13 +99,13 @@ func (c websocketClient) PluginHealth() (*scheme.PluginHealth, error) {
 // from/write to via the configured plugins.
 // It can be filtered to show only those devices which match a set
 // of provided tags by using ScanOptions.
-func (c websocketClient) Scan(opt scheme.ScanOptions) (*[]scheme.Scan, error) {
+func (c websocketClient) Scan(opts scheme.ScanOptions) (*[]scheme.Scan, error) {
 	return nil, nil
 }
 
 // Tags returns the list of all tags currently associated with devices.
 // If no TagsOptions is specified, the default tag namespace will be used.
-func (c websocketClient) Tags(opt scheme.TagsOptions) (*[]string, error) {
+func (c websocketClient) Tags(opts scheme.TagsOptions) (*[]string, error) {
 	return nil, nil
 }
 
@@ -72,29 +117,29 @@ func (c websocketClient) Info(id string) (*scheme.Info, error) {
 
 // Read returns data from devices which match the set of provided tags
 // using ReadOptions.
-func (c websocketClient) Read(opt scheme.ReadOptions) (*[]scheme.Read, error) {
+func (c websocketClient) Read(opts scheme.ReadOptions) (*[]scheme.Read, error) {
 	return nil, nil
 }
 
 // ReadDevice returns data from a specific device.
 // It is the same as Read() where the label matches the device id tag
 // specified in ReadOptions.
-func (c websocketClient) ReadDevice(id string, opt scheme.ReadOptions) (*[]scheme.Read, error) {
+func (c websocketClient) ReadDevice(id string, opts scheme.ReadOptions) (*[]scheme.Read, error) {
 	return nil, nil
 }
 
 // ReadCache returns stream reading data from the registered plugins.
-func (c websocketClient) ReadCache(opt scheme.ReadCacheOptions) (*[]scheme.Read, error) {
+func (c websocketClient) ReadCache(opts scheme.ReadCacheOptions) (*[]scheme.Read, error) {
 	return nil, nil
 }
 
 // WriteAsync writes data to a device, in an asynchronous manner.
-func (c websocketClient) WriteAsync(id string, opt []scheme.WriteData) (*[]scheme.Write, error) {
+func (c websocketClient) WriteAsync(id string, opts []scheme.WriteData) (*[]scheme.Write, error) {
 	return nil, nil
 }
 
 // WriteSync writes data to a device, waiting for the write to complete.
-func (c websocketClient) WriteSync(id string, opt []scheme.WriteData) (*[]scheme.Transaction, error) {
+func (c websocketClient) WriteSync(id string, opts []scheme.WriteData) (*[]scheme.Transaction, error) {
 	return nil, nil
 }
 
