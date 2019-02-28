786c0467285572c815d44f6e2a354c9cee987a15
Hoanh An <hoanhan@bennington.edu>
2019-02-28 15:50:05 -0500

    Describe request/response event

---
+++ synse/event.go
@@ -0,0 +1,11 @@
+package synse
+
+// event.go stores request/response events supported by the Synse WebSocket API.
+
+const (
+	// versionRequest describes the request/version event.
+	versionRequest = "request/version"
+
+	// versionResponse describes the response/version event.
+	versionResponse = "response/version"
+)
