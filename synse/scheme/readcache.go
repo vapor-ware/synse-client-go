87bbedf27d73d9dc0303a83064c8912d1154a690
Hoanh An <hoanhan@bennington.edu>
2019-05-12 18:23:32 -0400

    update annotations for readcache scheme

--- synse/scheme/readcache.go
+++ synse/scheme/readcache.go
@@ -2,6 +2,6 @@ package scheme
 
 // ReadCacheOptions describes the query parameters for `/readcache` endpoint.
 type ReadCacheOptions struct {
-	Start string
-	End   string
+	Start string `json:"start" yaml:"start" mapstructure:"start"`
+	End   string `json:"end" yaml:"end" mapstructure:"end"`
 }
