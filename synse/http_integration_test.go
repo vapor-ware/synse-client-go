1ecf627f2310c9e3bb071abfb6a3cd8bfb5008b5
Hoanh An <hoanhan@bennington.edu>
2019-08-02 17:03:03 +0700

    add version test

--- synse/http_integration_test.go
+++ synse/http_integration_test.go
@@ -26,3 +26,20 @@ func TestIntegration_Status(t *testing.T) {
 	assert.Equal(t, "ok", resp.Status)
 	assert.NotNil(t, resp.Timestamp)
 }
+
+func TestIntegration_Version(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	client, err := NewHTTPClientV3(&Options{
+		Address: "localhost:5000",
+	})
+	assert.NotNil(t, client)
+	assert.NoError(t, err)
+
+	resp, err := client.Version()
+	assert.NoError(t, err)
+	assert.Equal(t, "v3", resp.APIVersion)
+	assert.NotNil(t, resp.Version)
+}
