d281addeb4b64b4b612bd74ea00316b1db0415c3
Hoanh An <hoanhan@bennington.edu>
2019-08-02 17:23:25 +0700

    add config test

--- synse/http_integration_test.go
+++ synse/http_integration_test.go
@@ -43,3 +43,31 @@ func TestIntegration_Version(t *testing.T) {
 	assert.Equal(t, "v3", resp.APIVersion)
 	assert.NotNil(t, resp.Version)
 }
+
+func TestIntegration_Config(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	client, err := NewHTTPClientV3(&Options{
+		Address: "localhost:5000",
+	})
+	assert.NotNil(t, client)
+	assert.NoError(t, err)
+
+	resp, err := client.Config()
+	assert.NoError(t, err)
+	assert.Equal(t, "en_US", resp.Locale)
+	assert.Equal(t, "debug", resp.Logging)
+	assert.Equal(t, "emulator:5001", resp.Plugin.TCP[0])
+	assert.Empty(t, resp.Plugin.Unix)
+	assert.Empty(t, resp.Plugin.Discover)
+	assert.Equal(t, 0, resp.Cache.Device.TTL)
+	assert.Equal(t, 300, resp.Cache.Transaction.TTL)
+	assert.Equal(t, 3, resp.GRPC.Timeout)
+	assert.Empty(t, resp.GRPC.TLS.Cert)
+	assert.Equal(t, false, resp.Transport.HTTP) // FIXME - should this be true?
+	assert.Equal(t, false, resp.Transport.WebSocket)
+	assert.Equal(t, false, resp.Metrics.Enabled)
+	assert.Equal(t, true, resp.PrettyJSON)
+}
