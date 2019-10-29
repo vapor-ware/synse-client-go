378399662012ed1554b079c19baa5098c5348b63
Erick Daniszewski <edaniszewski@gmail.com>
2019-10-29 10:53:32 -0400

    clean up constants for websocket events

--- synse/event.go
+++ synse/event.go
@@ -3,97 +3,39 @@ package synse
 // event.go stores request/response events supported by the Synse WebSocket API.
 
 const (
-	// requestStatus describes the request/status event.
-	requestStatus = "request/status"
 
-	// requestVersion describes the request/version event.
-	requestVersion = "request/version"
-
-	// requestConfig describes the request/config event.
-	requestConfig = "request/config"
-
-	// requestPlugin describes the request/plugin event.
-	requestPlugin = "request/plugin"
-
-	// requestPlugins describes the request/plugins event.
-	requestPlugins = "request/plugins"
-
-	// requestPluginHealth describes the request/plugin_health event.
+	// Synse Server WebSocket API request events.
+	requestStatus       = "request/status"
+	requestVersion      = "request/version"
+	requestConfig       = "request/config"
+	requestPlugin       = "request/plugin"
+	requestPlugins      = "request/plugins"
 	requestPluginHealth = "request/plugin_health"
-
-	// requestScan describes the request/scan event.
-	requestScan = "request/scan"
-
-	// requestTags describes the request/tags event.
-	requestTags = "request/tags"
-
-	// requestInfo describes the request/info event.
-	requestInfo = "request/info"
-
-	// requestRead describes the request/read event.
-	requestRead = "request/read"
-
-	// requestReadDevice describes the request/read_device event.
-	requestReadDevice = "request/read_device"
-
-	// requestReadCache describes the request/read_cache event.
-	requestReadCache = "request/read_cache"
-
-	// requestWriteAsync describes the request/write_async event.
-	requestWriteAsync = "request/write_async"
-
-	// requestWriteSync describes the request/write_sync event.
-	requestWriteSync = "request/write_sync"
-
-	// requestTransactions describes the request/transactions event.
+	requestScan         = "request/scan"
+	requestTags         = "request/tags"
+	requestInfo         = "request/info"
+	requestRead         = "request/read"
+	requestReadDevice   = "request/read_device"
+	requestReadCache    = "request/read_cache"
+	requestReadStream   = "request/read_stream"
+	requestWriteAsync   = "request/write_async"
+	requestWriteSync    = "request/write_sync"
+	requestTransaction  = "request/transaction"
 	requestTransactions = "request/transactions"
 
-	// requestTransaction describes the request/transaction event.
-	requestTransaction = "request/transaction"
-
-	// responseStatus describes the response/status event.
-	responseStatus = "response/status"
-
-	// responseVersion describes the response/version event.
-	responseVersion = "response/version"
-
-	// responseConfig describes the response/config event.
-	responseConfig = "response/config"
-
-	// responsePlugin describes the response/plugin_info event.
-	responsePlugin = "response/plugin_info"
-
-	// responsePluginSummary describes the response/plugin_summary event.
-	responsePluginSummary = "response/plugin_summary"
-
-	// responsePluginHealth describes the response/plugin_health event.
-	responsePluginHealth = "response/plugin_health"
-
-	// responseTags describes the response/tags event.
-	responseTags = "response/tags"
-
-	// responseDevice describes the response/device event.
-	responseDevice = "response/device_info"
-
-	// responseDeviceSummary describes the response/device_summary event.
-	responseDeviceSummary = "response/device_summary"
-
-	// responseReading describes the response/reading event.
-	responseReading = "response/reading"
-
-	// FIXME (etd) - will need to rename some of these
-	// responseWriteAsync describes the response/write_async event.
-	responseWriteAsync = "response/transaction_info"
-
-	// responseWriteSync describes the response/write_sync event.
-	responseWriteSync = "response/transaction_status"
-
-	// responseTransactionList describes the response/transaction_list event.
-	responseTransactionList = "response/transaction_list"
-
-	// responseTransactionInfo describes the response/transaction_info event.
-	responseTransactionInfo = "response/transaction_status"
-
-	// responseError describes the response/error event.
-	responseError = "response/error"
+	// Synse Server WebSocket API response events.
+	responseStatus            = "response/status"
+	responseVersion           = "response/version"
+	responseConfig            = "response/config"
+	responsePluginInfo        = "response/plugin_info"
+	responsePluginSummary     = "response/plugin_summary"
+	responsePluginHealth      = "response/plugin_health"
+	responseDeviceSummary     = "response/device_summary"
+	responseTags              = "response/tags"
+	responseDeviceInfo        = "response/device_info"
+	responseReading           = "response/reading"
+	responseTransactionInfo   = "response/transaction_info"
+	responseTransactionStatus = "response/transaction_status"
+	responseTransactionList   = "response/transaction_list"
+	responseError             = "response/error"
 )
